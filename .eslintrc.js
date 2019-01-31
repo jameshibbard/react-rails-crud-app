0f44ba7746430af2d932def2fec188b5232bc366
James Hibbard <1940994+jameshibbard@users.noreply.github.com>
2019-01-31 14:55:19 +0100

    Added ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: 'airbnb',
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'no-console': 0,
+    'no-alert': 0,
+  },
+};
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,9 @@
-import React from "react";
-import { render } from "react-dom";
-import App from "../components/App";
+/* global document */
 
-document.addEventListener("DOMContentLoaded", () => {
-  render(<App />, document.querySelector("#root"));
+import React from 'react';
+import { render } from 'react-dom';
+import App from '../components/App';
+
+document.addEventListener('DOMContentLoaded', () => {
+  render(<App />, document.querySelector('#root'));
 });
