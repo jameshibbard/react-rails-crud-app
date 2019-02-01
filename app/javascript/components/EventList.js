99ad3b8035a791fc92ead97f8d5545ea40a35d9e
James Hibbard <1940994+jameshibbard@users.noreply.github.com>
2019-02-01 11:51:13 +0100

    Added search

--- app/javascript/components/EventList.js
+++ app/javascript/components/EventList.js
@@ -1,13 +1,42 @@
+/* eslint-disable camelcase */
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class EventList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+    };
+
+    this.searchInput = React.createRef();
+    this.updateSearchTerm = this.updateSearchTerm.bind(this);
+  }
+
+  updateSearchTerm() {
+    this.setState({ searchTerm: this.searchInput.current.value });
+  }
+
+  matchSearchTerm(obj) {
+    const {
+      id, published, created_at, updated_at, ...rest
+    } = obj;
+    const { searchTerm } = this.state;
+
+    return Object.values(rest).some(
+      value => value.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1,
+    );
+  }
+
   renderEvents() {
     const { activeId, events } = this.props;
-    events.sort((a, b) => new Date(b.event_date) - new Date(a.event_date));
+    const filteredEvents = events
+      .filter(el => this.matchSearchTerm(el))
+      .sort((a, b) => new Date(b.event_date) - new Date(a.event_date));
 
-    return events.map(event => (
+    return filteredEvents.map(event => (
       <li key={event.id}>
         <Link to={`/events/${event.id}`} className={activeId === event.id ? 'active' : ''}>
           {event.event_date}
@@ -22,9 +51,18 @@ class EventList extends React.Component {
     return (
       <section className="eventList">
         <h2>
-          Events
+        Events
           <Link to="/events/new">New Event</Link>
         </h2>
+
+        <input
+          className="search"
+          placeholder="Search"
+          type="text"
+          ref={this.searchInput}
+          onKeyUp={this.updateSearchTerm}
+        />
+
         <ul>{this.renderEvents()}</ul>
       </section>
     );
--- app/javascript/components/Header.js
+++ app/javascript/components/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
   <header>
-    <h1>Event Manager</h1>
+    <h1>
+      <Link to="/events">Event Manager</Link>
+    </h1>
   </header>
 );
 
